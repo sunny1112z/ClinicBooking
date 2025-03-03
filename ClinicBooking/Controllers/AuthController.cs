e0f1304053ec62679761c88a031cf3f3c18f0428
sunny1112w <duyn2280z@gmail.com>
2025-03-03 23:51:53 +0700

    Update

--- ClinicBooking/Controllers/AuthController.cs
+++ ClinicBooking/Controllers/AuthController.cs
@@ -1,16 +1,58 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using ClinicBooking.
 namespace ClinicBooking.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
         // GET: AuthController
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var user = await _authService.AuthenticateUser(email, password);
+            var userRole = await _authService.GetIsActiveByEmail(email);
+            if (user == null)
+            {
+                ViewBag.Error = "Invalid email or password";
+                return View();
+            }
+            if (userRole == 1)
+            {
+                HttpContext.Session.SetInt32("UserId", user.AccountId);
+                HttpContext.Session.SetInt32("UserRole", user.AccountRole ?? -1);
 
+                if (user.AccountRole == 0)
+                {
+                    return RedirectToAction("Index", "Account");
+                }
+                if (user.AccountRole == 1)
+                {
+                    return RedirectToAction("Index", "Category");
+                }
+                if (user.AccountRole == 2)
+                {
+                    return RedirectToAction("Manage", "NewsArticle");
+                }
+
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Your account has been locked";
+                return View("CannotLogin");
+
+            }
+            return RedirectToAction("Index", "NewsArticle");
+        }
         // GET: AuthController/Details/5
         public ActionResult Details(int id)
         {
